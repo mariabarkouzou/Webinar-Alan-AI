c052040baccf16d14559c036c415c6d4d186a8c1
Maria Barkouzou <m.mparkouzou@yahoo.gr>
2021-10-11 16:37:34 +0300

    Project Integrate Alan AI ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import alanBtn from "@alan-ai/alan-sdk-web";
+import React, { useEffect } from "react";
+
+
+const alanKey = process.env.REACT_APP_API_KEY;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+    });
+  }, []);
+
+  return <div className="App"></div>;
 }
 
 export default App;
